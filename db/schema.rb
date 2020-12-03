18d6f65f33d5fe2ef74e6e06be76619743d5bf1d
Fabian Alvarez <fabian.alvarezch@gmail.com>
2020-12-02 21:24:16 -0600

    Changed database src to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_11_30_002207) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_002207) do
     t.string "title"
     t.text "description"
     t.float "price"
-    t.integer "host_id", null: false
+    t.bigint "host_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["host_id"], name: "index_properties_on_host_id"
@@ -64,8 +67,8 @@ ActiveRecord::Schema.define(version: 2020_11_30_002207) do
     t.integer "guests"
     t.float "total_cost"
     t.string "approved", default: "pending", null: false
-    t.integer "property_id", null: false
-    t.integer "host_id", null: false
+    t.bigint "property_id", null: false
+    t.bigint "host_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["host_id"], name: "index_reservations_on_host_id"
